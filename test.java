cf773dae0c305746d54eda84651145bf5842c877
a62541398 <ling6253112000@yahoo.com.tw>
2015-02-26 16:57:37 +0800

    java_七言

---
+++ test.java
@@ -0,0 +1,10 @@
+class test {
+  public static void main(String[] args)  {
+    System.out.println("《送孟浩然之廣陵》");
+System.out.println("作者：李白");
+System.out.println("故人西辭黃鶴樓，");
+System.out.println("煙花三月下揚州。");
+System.out.println("孤帆遠影碧空盡，");
+System.out.println("惟見長江天際流。");
+  }
+}
